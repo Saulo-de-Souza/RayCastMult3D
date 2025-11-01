57db3c3fa029066203824801bf18688372a5ff6c
Saulo-de-Souza <saulodevbr@gmail.com>
2025-11-01 18:28:57 -0300

    Add docs

--- addons/raycast_mult_3d/raycast_mult_3d.gd
+++ addons/raycast_mult_3d/raycast_mult_3d.gd
@@ -6,57 +6,16 @@ var _mesh_instance_debug: MeshInstance3D = MeshInstance3D.new()
 var _cylinder_mesh_debug: CylinderMesh = CylinderMesh.new()
 var _material_debug: StandardMaterial3D = StandardMaterial3D.new()
 
-@export_subgroup("Params")
-@export var collide_with_areas: bool = false:
-	set(value):
-		collide_with_areas = value
-		if is_instance_valid(_params): _params.collide_with_areas = value
-@export var collide_with_bodies: bool = true:
-	set(value):
-		collide_with_bodies = value
-		if is_instance_valid(_params): _params.collide_with_bodies = value
-@export var show_debug: bool = true
-@export var max_result: int = 10
-@export_range(0.0, 10.0, 0.01) var offset: float = 0.0
-@export_range(0.0001, 0.1, 0.0001) var margin: float = 0.001
-@export var hit_back_faces: bool = true:
-	set(value):
-		hit_back_faces = value
-		if is_instance_valid(_params):
-			_params.hit_back_faces = value
-@export var hit_from_inside: bool = false:
-	set(value):
-		hit_from_inside = value
-		if is_instance_valid(_params):
-			_params.hit_from_inside = value
-
-@export_subgroup("Collision Mask")
-@export_flags_3d_physics var collision_mask = (1 << 0):
-	set(value):
-		collision_mask = value
-		if is_instance_valid(_params): _params.collision_mask = value
-	
-@export_subgroup("Excludes")
-@export var _excludes: Array[PhysicsBody3D] = []:
-	set(value):
-		_excludes = value
-		if is_instance_valid(_params):
-			var new_excludes: Array[RID] = []
-			for b in _excludes:
-				if b:
-					new_excludes.append(b.get_rid())
-			if exclude_from and not from == null and from.has_method("get_rid"):
-				new_excludes.append(from.get_rid())
-			if exclude_to and not to == null and to.has_method("get_rid"):
-				new_excludes.append(to.get_rid())
-			_params.exclude = new_excludes
 
 @export_subgroup("From")
+## Source of the raycast. Example: a Marker3D or a Node3D. 
 @export var from: Node3D:
 	set(value):
 		from = value
 		update_configuration_warnings()
+## Deviation from the raycast origin.
 @export var from_offset: Vector3 = Vector3.ZERO
+## Excludes the source node so it won't be detected.
 @export var exclude_from: bool = true:
 	set(value):
 		exclude_from = value
@@ -71,11 +30,14 @@ var _material_debug: StandardMaterial3D = StandardMaterial3D.new()
 		_params.exclude = new_excludes
 
 @export_subgroup("To")
+## Raycast destination. Example: a Marker3D or a Node3D.
 @export var to: Node3D:
 	set(value):
 		to = value
 		update_configuration_warnings()
+## Deviation from the raycast destination.
 @export var to_offset: Vector3 = Vector3.ZERO
+## Excludes the destination node so it cannot be detected.
 @export var exclude_to: bool = true:
 	set(value):
 		exclude_to = value
@@ -89,18 +51,74 @@ var _material_debug: StandardMaterial3D = StandardMaterial3D.new()
 			new_excludes.append(to.get_rid())
 		_params.exclude = new_excludes
 
+@export_subgroup("Params")
+## Select this option if you want the raycast to detect 3D areas.
+@export var collide_with_areas: bool = false:
+	set(value):
+		collide_with_areas = value
+		if is_instance_valid(_params): _params.collide_with_areas = value
+## Select this option if you want the raycast to detect 3D bodies.
+@export var collide_with_bodies: bool = true:
+	set(value):
+		collide_with_bodies = value
+		if is_instance_valid(_params): _params.collide_with_bodies = value
+## Exibe no editor o raycast 3d.
+@export var show_debug: bool = true
+## The amount of raycast penetration, that is, how many detections you want to receive.
+@export var max_result: int = 10
+## Increase the Raycast tips.
+@export_range(0.0, 10.0, 0.01) var offset: float = 0.0
+## This is the margin that raycast considers when detecting 3D bodies and areas in space. For example, if one object is very close to another object, a smaller margin will detect both objects better.
+@export_range(0.0001, 0.1, 0.0001) var margin: float = 0.001
+## If true, the query will hit back faces with concave polygon shapes with back face enabled or heightmap shapes.
+@export var hit_back_faces: bool = true:
+	set(value):
+		hit_back_faces = value
+		if is_instance_valid(_params):
+			_params.hit_back_faces = value
+## If true, the query will detect a hit when starting inside shapes. In this case the collision normal will be Vector3(0, 0, 0). Does not affect concave polygon shapes or heightmap shapes.
+@export var hit_from_inside: bool = false:
+	set(value):
+		hit_from_inside = value
+		if is_instance_valid(_params):
+			_params.hit_from_inside = value
 
+@export_subgroup("Collision Mask")
+## The physics layers the query will detect (as a bitmask). By default, all collision layers are detected. See Collision layers and masks   in the documentation for more information.
+@export_flags_3d_physics var collision_mask = (1 << 0):
+	set(value):
+		collision_mask = value
+		if is_instance_valid(_params): _params.collision_mask = value
+	
+@export_subgroup("Excludes")
+## The list of object RIDs that will be excluded from collisions. Use CollisionObject3D.get_rid() to get the RID associated with a CollisionObject3D-derived node. Note: The returned array is copied and any changes to it will not update the original property value. To update the value you need to modify the returned array, and then assign it to the property again.
+@export var _excludes: Array[PhysicsBody3D] = []:
+	set(value):
+		_excludes = value
+		if is_instance_valid(_params):
+			var new_excludes: Array[RID] = []
+			for b in _excludes:
+				if b:
+					new_excludes.append(b.get_rid())
+			if exclude_from and not from == null and from.has_method("get_rid"):
+				new_excludes.append(from.get_rid())
+			if exclude_to and not to == null and to.has_method("get_rid"):
+				new_excludes.append(to.get_rid())
+			_params.exclude = new_excludes
+
+
+## Signal emitted by detected 3D bodies and areas.
 signal intersect_ray(results: Array[RaycastMultResult])
 
 
 func _get_configuration_warnings() -> PackedStringArray:
 	var warnings = []
 	if from == null:
-		warnings.append("Favor escolher um Node3D para o parâmetro from")
+		warnings.append("Please select a Node3D for the from parameter.")
 	if to == null:
-		warnings.append("Favor escolher um Node3D para o parâmetro to")
+		warnings.append("Please select a Node3D for the parameter to.")
 	if not to == null and to == from:
-		warnings.append("As propriedades from e to não podem ter o mesmo node")
+		warnings.append("The properties from and to cannot have the same node.")
 	return warnings
 
 
@@ -259,7 +277,7 @@ func _update_mesh_visible() -> void:
 	else:
 		_mesh_instance_debug.visible = true
 		
-			
+## Add an area or a 3D body to be excluded from raycast detections.			
 func add_exclude(_exclude: PhysicsBody3D) -> void:
 	if _exclude == null:
 		return
@@ -280,7 +298,7 @@ func add_exclude(_exclude: PhysicsBody3D) -> void:
 			new_excludes.append(to.get_rid())
 		_params.exclude = new_excludes
 
-	
+## Removes an area or a 3D body so that it is not excluded from raycast detections.	
 func remove_exclude(_exclude: PhysicsBody3D) -> void:
 	if _excludes.has(_exclude):
 		_excludes.erase(_exclude)
